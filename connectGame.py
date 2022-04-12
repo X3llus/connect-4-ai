54b0aca0a4956f2e9e9c69a8f97da2bf5fb41d22
Kyle Lozinski <kclozins@lakeheadu.ca>
2022-04-11 23:03:25 -0400

    Created Game
    Created game - finished up until checking for win - checking done for vertical and horizontal - not for diagonal

---
+++ connectGame.py
@@ -0,0 +1,69 @@
+from ast import If
+from tkinter import END
+import numpy as np
+
+class ConnectGame:
+    board = np.zeros((5,5))
+    playerPiece = 1
+    turnsTaken = 0
+
+    def getBoard(self):
+        return self.board
+    
+    def playPiece(self, x):
+        if x < 0 or x > 4:
+            return False
+        elif self.board[0][x] == 0:
+            self.__placePiece(x)
+            return True
+        else:
+            return False
+            
+    def __placePiece(self, x):
+        for y in range (4, -1, -1):
+            if self.board[y][x] == 0:
+                self.board[y][x] = self.playerPiece
+                if self.__checkSolved(x, y):
+                    exit()
+                self.__swapTurn()
+                return True
+        return False
+
+    def __swapTurn(self):
+        if self.playerPiece == 1:
+            self.playerPiece = 2
+        else:
+            self.playerPiece = 1
+        self.turnsTaken += 1
+        print(self.getBoard())
+
+    def __checkSolved(self, x, y):
+        if self.__solvingAlgorythm(x, y, self.playerPiece) == True:
+            print("Weenor")
+        else:
+            print("Tiny Weenor")
+
+    #Can be passed 1 - 8 for direction, 1 is 12 O-Clock, rest are clockwise -- Use 0 when starting sequence
+    #Use 0 for line count when starting as well
+    def __solvingAlgorythm(self, x, y, player):
+        foundCount = 0
+        for y in range (5):
+            for x in range (5):
+                if self.board[x][y] == player:
+                    foundCount += 1
+                else:
+                    if foundCount > 0:
+                        break
+        if foundCount > 3:
+            return True
+        foundCount = 0
+        for x in range (5):
+            for y in range (5):
+                if self.board[x][y] == player:
+                    foundCount += 1
+                else:
+                    if foundCount > 0:
+                        break
+        if foundCount > 3:
+            return True
+        return False
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from connectGame import ConnectGame
 
 def main():
-    print('running!')
+    game = ConnectGame()
+    for i in range (9):
+        if i % 2 == 0:
+            game.playPiece(2)
+        else:
+            game.playPiece(3)
 
 if __name__ == '__main__':
     main()
