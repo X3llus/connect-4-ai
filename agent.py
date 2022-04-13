2af2201cf12805eea3c4994cbd46662cd9c97889
Kyle Lozinski <kclozins@lakeheadu.ca>
2022-04-13 19:50:10 -0400

    Update agent.py

--- agent.py
+++ agent.py
@@ -15,22 +15,42 @@ class Agent:
         self.epsilon = 0 #randomness
         self.gamma = 0 #discount rate
         self.memory = deque(maxlen=MAX_MEMORY) # popleft()
+        self.model = None # TODO
+        self.trainer = None # TODO
         # TODO: model, trainer
 
     def get_state(self, game):
-        pass
+        state = game.getBoard.flatten(order='C')
 
-    def remember(self, state, action, reward, next_state, gameOver):
-        pass
+    def remember(self, state, action, reward, next_state, game_over):
+        self.memory.append((state, action, reward, next_state, game_over)) # will popleft if MAX_MEMORY is reached
 
     def train_long_memory(self):
-        pass
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE) # list of tuples
+        else:
+            mini_sample = self.memory
+        
+        states, actions, rewards, next_states, game_overs = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, game_overs)
 
-    def train_short_memory(self, state, action, reward, next_state, gameOver):
-        pass
+    def train_short_memory(self, state, action, reward, next_state, game_over):
+        self.trainer.train_step(state, action, reward, next_state, game_over)
 
     def get_action(self, state):
-        pass
+        # random moves: tradeoff exploration / exploitation
+        self.epsilon = 80 - self.n_games # allowing epsilon to decrease over time
+        final_move = [0,0,0]
+        if random.randint(0, 200) < self.epsilon: # randomly generating if move should be random
+            move = random.randint(0, 2) # random value 0-1
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model.predict(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+        
+        return final_move
 
 def train():
     plot_scores = []
